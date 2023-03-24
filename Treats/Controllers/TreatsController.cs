a5e9eaf1cf01a61b2584cb111a6c0361b8d870c1
E Luckie <eluckie.d@gmail.com>
2023-03-24 14:55:27 -0600

    add Index view & path for Treats

--- Treats/Controllers/TreatsController.cs
+++ Treats/Controllers/TreatsController.cs
@@ -1,25 +1,52 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Treats.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using System.Security.Claims;
 
 namespace Treats.Controllers
 {
+  [Authorize]
   public class TreatsController : Controller
   {
     private readonly TreatsContext _db;
     private readonly UserManager<AppUser> _userManager;
     public TreatsController(UserManager<AppUser> userManager, TreatsContext db)
-      {
-        userManager = _userManager;
-        db = _db;
-      }
-    public ActionResult Index()
     {
-      return View();
+      _userManager = userManager;
+      _db = db;
+    }
+    [AllowAnonymous]
+    public async Task<ActionResult> Index()
+    {
+      string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      AppUser currentUser = await _userManager.FindByIdAsync(userId);
+      ViewBag.PageTitle = "Treats";
+      if (currentUser != null)
+      {
+        List<Treat> userTreats = _db.Treats
+          .Where(entry => entry.User.Id == currentUser.Id)
+          .OrderByDescending(treat => treat.Name)
+          .Include(treat => treat.Join)
+          .ThenInclude(join => join.Flavor)
+          .ToList();
+        ViewBag.Nickname = currentUser.Nickname;
+        return View(userTreats);
+      }
+      else
+      {
+        List<Treat> allTreats = _db.Treats
+          .OrderByDescending(treat => treat.Name)
+          .Include(treat => treat.Join)
+          .ThenInclude(join => join.Flavor)
+          .ToList();
+        return View(allTreats);
+      }
     }
   }
 }
