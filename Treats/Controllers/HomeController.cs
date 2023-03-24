824c27482dc9d0484ca4e86e397e68b0af4d0da8
E Luckie <eluckie.d@gmail.com>
2023-03-24 11:01:09 -0600

    add Home Index path & view

--- Treats/Controllers/HomeController.cs
+++ Treats/Controllers/HomeController.cs
@@ -13,12 +13,24 @@ namespace Treats.Controllers
     private readonly TreatsContext _db;
     private readonly UserManager<AppUser> _userManager;
     public HomeController(UserManager<AppUser> userManager, TreatsContext db)
-      {
-        userManager = _userManager;
-        db = _db;
-      }
-    public ActionResult Index()
     {
+      _userManager = userManager;
+      _db = db;
+    }
+    [HttpGet("/")]
+    public async Task<ActionResult> Index()
+    {
+      // string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      // AppUser currentUser = await _userManager.FindByIdAsync(userId);
+      // ViewBag.PageTitle = "Pierre's Sweet and Savory Treats";
+      // if (currentUser != null)
+      // {
+      //   ViewBag.Nickname = currentUser.Nickname;
+      //   List<Treat> treats = _db.Treats
+      //     .Where(entry => entry.User.Id == currentUser.Id)
+      //     .ToList();
+      //   return View(treats);
+      // }
       return View();
     }
   }
