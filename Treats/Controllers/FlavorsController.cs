da730881b7381090ccee195cfa6d2b2060562674
E Luckie <eluckie.d@gmail.com>
2023-03-24 19:44:16 -0600

    add functionality to DeleteJoin from Flavors

--- Treats/Controllers/FlavorsController.cs
+++ Treats/Controllers/FlavorsController.cs
@@ -121,5 +121,13 @@ namespace Treats.Controllers
       }
       return RedirectToAction("Details", new { id = flavor.FlavorId });
     }
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      TreatFlavor join = _db.TreatFlavors.FirstOrDefault(join => join.TreatFlavorId == joinId);
+      _db.TreatFlavors.Remove(join);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
